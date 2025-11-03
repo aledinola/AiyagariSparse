4f72337bf5fc0569d4204b7fddcf5ed8ab27857b
Alessandro Di Nola <40523651+aledinola@users.noreply.github.com>
2025-11-03 17:05:29 +0000

    fixed bugs in low memory

--- ValueFnIter_LowMem_nod_raw_sparse.m
+++ ValueFnIter_LowMem_nod_raw_sparse.m
@@ -6,8 +6,16 @@ N_a=prod(n_a);
 N_z=prod(n_z);
 
 Policy=zeros(N_a,N_z,'gpuArray');
-
 special_n_z=ones(l_z,1);
+
+% Precompute variables for Howard improvement step
+Ftemp          = zeros(N_a,N_z,'gpuArray');
+pi_z_transpose = transpose(pi_z);
+NaVec = gpuArray.colon(1,N_a)';
+NzVec = gpuArray.colon(1,N_z)';
+a_ind = repmat(NaVec,N_z,1);
+z_ind = repelem(NzVec,N_a,1);
+ind   = a_ind+(z_ind-1)*N_a;
 
 %%
 tempcounter=1;
@@ -30,6 +38,10 @@ while currdist>Tolerance && tempcounter<=maxiter
         [Vtemp,maxindex]=max(entireRHS,[],1);
         VKron(:,z_c)=Vtemp;
         Policy(:,z_c)=maxindex;
+
+        tempmaxindex=maxindex+(0:1:N_a-1)*N_a;
+        Ftemp(:,z_c)=ReturnMatrix_z(tempmaxindex); 
+
     end %end z
     
     VKrondist=VKron(:)-VKronold(:);
@@ -40,11 +52,9 @@ while currdist>Tolerance && tempcounter<=maxiter
     % Use Howards Policy Fn Iteration Improvement 
     % (except for first few and last few iterations, as it is not a good idea there)
     if isfinite(currdist) && currdist/Tolerance>10 && tempcounter<MaxHowards
-       % Reshape Policy as column vector with size [N_a*N_z,1] 
-        Policy_vec=reshape(Policy,[N_a*N_z,1]); 
-        % Linear indices of the elements you want in ReturnMatrix
-        linIdx = Policy_vec + (a_ind-1)*N_a + (z_ind-1)*N_a*N_a;
-        Ftemp = ReturnMatrix(linIdx);
+       
+        Policy_vec = reshape(Policy,[N_a*N_z,1]);
+        Ftemp_vec  = reshape(Ftemp,[N_a*N_z,1]);
         % Build large sparse matrix
         indp  = Policy_vec+(z_ind-1)*N_a;
         Qmat  = sparse(ind,indp,1,N_a*N_z,N_a*N_z);
@@ -52,7 +62,7 @@ while currdist>Tolerance && tempcounter<=maxiter
         for Howards_counter=1:Howards
             EV_howard = VKron*pi_z_transpose; % (a',z)
             EV_howard = reshape(EV_howard,[N_a*N_z,1]);
-            VKron = Ftemp+DiscountFactorParamsVec*Qmat*EV_howard;
+            VKron = Ftemp_vec+beta*Qmat*EV_howard;
             VKron = reshape(VKron,[N_a,N_z]);
         end
     end
@@ -60,7 +70,7 @@ while currdist>Tolerance && tempcounter<=maxiter
     tempcounter=tempcounter+1;
 end
 
-Policy=PolicyIndexes;
+Policy=reshape(Policy,[N_a,N_z]);
 
 
 
